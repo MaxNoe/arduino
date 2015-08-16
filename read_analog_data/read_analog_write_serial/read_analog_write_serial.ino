38a521f7d98f3159b2e50b6d3082ecf2e42021fd
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-08-16 15:22:29 +0200

    use nanopb on the arduino

--- read_analog_data/read_analog_write_serial/read_analog_write_serial.ino
+++ read_analog_data/read_analog_write_serial/read_analog_write_serial.ino
@@ -1,6 +1,11 @@
+#include <Time.h>
 #include <pb.h>
 #include <pb_encode.h>
+#include <adcvalues.pb.h>
 
+unsigned short sensorValue0;
+unsigned long runtime;
+unsigned int unixtime;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,12 +16,17 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  unsigned int sensorValue0 = analogRead(A0);
-  unsigned long t = millis();
+  sensorValue0 = analogRead(A0);
+  runtime = millis();
+  unixtime = now();
   
-  Serial.print("{\"t\":");  
-  Serial.print(t);
-  Serial.print(", \"adc\":");
-  Serial.print(sensorValue0);
-  Serial.println("}");
-}
+  SerialData message = {runtime, unixtime, sensorValue0};
+  
+  uint8_t buffer[16];
+  pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+  pb_encode(&stream, SerialData_fields, &message);
+  for(int i=0; i < stream.bytes_written; i++){
+    Serial.write(buffer[i]);
+  }
+  Serial.write("\n");
+ }
